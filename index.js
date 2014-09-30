37d5211cde0f4fba1ec516aa414dfb9a7c676c2a
Florent Cailhol <florent.cailhol@gmail.com>
2014-09-30 11:22:43 +0200

    Remove myth

--- index.js
+++ index.js
@@ -2,16 +2,22 @@
 
 var _ = require('lodash')
 var es = require('event-stream')
+var autoprefixer = require('gulp-autoprefixer')
 var concat = require('gulp-concat')
 var rework = require('gulp-rework')
 
 // rework tasks
-var myth = require('myth')
+var calc = require('rework-calc')
+var color = require('rework-color-function')
+var media = require('rework-custom-media')
 var ielimits = require('rework-ie-limits')
 var inherit = require('rework-inherit')
-var macro   = require('rework-macro')
+var macro = require('rework-macro')
 var npm = require('rework-npm')
+var vars = require('rework-vars')
 var convert = require('./lib/rework-convert-units')
+
+exports.autoprefixer = autoprefixer
 
 exports.rework = function(options) {
   options = _.defaults(options || {}, {
@@ -20,13 +26,11 @@ exports.rework = function(options) {
 
   return rework(
     npm({path: options.path}),
+    vars(),
     macro,
-    myth({
-      fontVariant:   false,
-      hexAlpha:      false,
-      import:        false,
-      rebecaapurple: false,
-    }),
+    calc,
+    color,
+    media(),
     inherit(),
     convert(options.fontSize),
     ielimits
